d3abdb55c1d979c065023f11ddd19150706e4250
awerito1994 <alex_awuero@hotmail.com>
2019-10-18 18:51:29 -0500

    cambios realizados

---
+++ introJava/src/Operadores.java
@@ -0,0 +1,31 @@
+/*
+ Esta clase va ha servir para trabajar con las distintas clases de operadores
+ en java
+ */
+
+/**
+ *
+ * @author Salas
+ */
+public class Operadores {
+    
+    public static void main(String[] args) {
+        //1.OPERADORES ARITMETICOS
+        int suma = 14 + 14;
+        int resta = 20 - 10;
+        double multiplicacion  = 50 * 0.12;
+        int division = 100 / 50;
+        int residuo = 7 % 2;
+        
+        System.out.println("La suma es:" + suma);
+        System.out.println("La resta es;" + resta);
+        System.out.println("La multiplicacion es;" + multiplicacion);
+        System.out.println("La division es;" + division);
+        System.out.println("El residuo es;" + residuo);
+        
+        
+        //2. OPERADORES DE INCREMENTO Y DECREMENTO
+        
+    }
+   
+}
---
+++ introJava/src/introjava/IntroJava.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author Salas
+ */
+public class IntroJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("bienvenidos a la propgramacion en java");
+        System.out.println("este codigo va ha ser subido en github en el repositorio1920");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
