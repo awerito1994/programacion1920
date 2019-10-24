6ba7de2f9cd87ced20114f082e94f2abce9df850
awerito1994 <alex_awuero@hotmail.com>
2019-10-23 22:23:21 -0500

    Create Areadeuntriaguloclase3.java

---
+++ introJava/src/introjava/Areadeuntriaguloclase3.java
@@ -0,0 +1,35 @@
+/*
+ Aquí vamos a calcular la area de un triángulo
+ */
+package introjava;
+import java.util.Scanner;
+        
+
+/**
+ *
+ * @author POWER COMPUTERS
+ */
+public class Areadeuntriaguloclase3 {
+    
+    public static void main(String[] args) {
+        
+        double area;
+        double base;
+        double altura;
+        
+        System.out.println("Ingrese el base");
+        Scanner entrada=new Scanner (System.in);
+        base=entrada.nextDouble();
+        
+        System.out.println("Ingrese la altura");
+        Scanner entrada1=new Scanner (System.in);
+        altura=entrada1.nextDouble();
+        
+        area=(base*altura)/2;
+        
+        
+        System.out.println("El area del triangulo es: " + area);
+        
+    }
+    
+}
