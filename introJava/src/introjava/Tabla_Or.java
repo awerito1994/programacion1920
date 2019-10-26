0e84ab6d619fc34793a67ea3ac0ddfb74d42beb3
awerito1994 <alex_awuero@hotmail.com>
2019-10-25 19:05:01 -0500

    operaciones con tabla or

---
+++ introJava/src/introjava/Tabla_Or.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author Salas
+ */
+public class Tabla_Or {
+
+    public static void main(String[] args) {
+        System.out.println("TABLA DE VERDAD OR ");
+        System.out.println("x \t y \t resultado");
+        System.out.println("___________________________");
+        System.out.println("y \t V \t" + (true || true));
+        System.out.println("___________________________");
+        System.out.println("y \t f \t" + (true || false));
+        System.out.println("___________________________");
+        System.out.println("f \t V \t" + (false || true));
+        System.out.println("___________________________");
+        System.out.println("f \t f \t" + (false || false));
+        System.out.println("___________________________");
+
+        System.out.println("TABLA DE VERDAD NOT");
+        System.out.println("x \t resultado");
+        System.out.println(true + "\t" + !true);
+        System.out.println(false + "\t" + !false);
+
+    }
+
+}
