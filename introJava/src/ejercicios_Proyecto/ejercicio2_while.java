8295133e6eed4354d2515168152239d5d53b594f
awerito1994 <alex_awuero@hotmail.com>
2019-11-28 15:53:06 -0500

    Update ejercicio2_while.java

--- introJava/src/ejercicios_Proyecto/ejercicio2_while.java
+++ introJava/src/ejercicios_Proyecto/ejercicio2_while.java
@@ -10,6 +10,7 @@ package ejercicios_Proyecto;
  * @author POWER COMPUTERS
  */
 public class ejercicio2_while {
+
     public static void main(String[] args) {
         int cont = 1;
         int acumulador = 1;
@@ -18,23 +19,23 @@ public class ejercicio2_while {
         System.out.println("LOS 4 PRIMEROS NUMEROS PERFECTOS ");
         System.out.println("_____________________________________________________");
         while (cont <= max) {
-            
+
             for (cont = 1; cont <= 10000;) {    // contador es el número que vamos a comprobar
                 cont++;
                 suma = 0;
                 for (acumulador = 1; acumulador < cont; acumulador++) {  // Amcumulador son los divisores. Se divide desde 1 hasta contador-1
-                    
+
                     if (cont % acumulador == 0) {
                         suma = suma + acumulador;     // si es divisor se suma
                     }
                 }
                 if (cont == suma) {           // si el numero es igual a la suma de sus divisores es perfecto
                     System.out.println("El siguiente numero es un numero perfecto :" + cont);
-                    
+
                            
                 }
             }
         }
     }
-    }
+}
     
