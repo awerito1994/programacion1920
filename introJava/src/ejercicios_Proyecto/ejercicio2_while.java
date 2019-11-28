69adfd94b9707d0cc09a0bbadd255c88aa359a8b
awerito1994 <alex_awuero@hotmail.com>
2019-11-28 17:22:14 -0500

    Update ejercicio2_while.java

--- introJava/src/ejercicios_Proyecto/ejercicio2_while.java
+++ introJava/src/ejercicios_Proyecto/ejercicio2_while.java
@@ -5,8 +5,6 @@
  */
 package ejercicios_Proyecto;
 
-
-
 /**
  *
  * @author POWER COMPUTERS
@@ -14,30 +12,29 @@ package ejercicios_Proyecto;
 public class ejercicio2_while {
 
     public static void main(String[] args) {
-      int it = 1;
+        int it = 1;
         int cont = 0;
         int num = 1;
         int suma_div = 0;
-       
 
         System.out.println("Los 4 primeros números perfectos son: ");
         while (cont < 4) {
             while (it < num) {
                 if ((num % it) == 0) {
-                    suma_div +=  it;
+                    suma_div += it;
                 }
                 it++;
             }
-            
-            if(suma_div==num){
-                System.out.println("El siguiente número es numero perfecto: "+num);
+
+            if (suma_div == num) {
+                System.out.println("El siguiente número es numero perfecto: " + num);
                 cont++;
             }
-            suma_div=0;
-            it=1;
+            suma_div = 0;
+            it = 1;
             num++;
         }
         System.out.println("");
-    
+
     }
 }
