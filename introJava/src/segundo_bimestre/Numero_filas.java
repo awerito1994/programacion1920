bf4c815d74a28560c380e5296ba436a2b1853bee
awerito1994 <alex_awuero@hotmail.com>
2019-12-05 15:59:40 -0500

    Create Numero_filas.java

---
+++ introJava/src/segundo_bimestre/Numero_filas.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundo_bimestre;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author POWER COMPUTERS
+ */
+public class Numero_filas {
+    public static void main(String[] args) {
+        
+      
+        int num;
+        int cont =1;
+        String piramide = "";
+        Scanner entrada = new  Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        num = entrada.nextInt();
+        System.out.println("----------------------------------------");
+        while(cont <= num){
+            piramide += cont +",";
+            System.out.println(""+piramide);
+            cont++;
+        }
+                
+    }
+    
+}
